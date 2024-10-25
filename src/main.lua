c202b179fe04e761a28fe0c1ae1a1d9805b99797
deissh <deissh@yandex.ru>
2024-10-25 22:44:04 +0300

    refactor: recreate init scripts

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,7 @@
-require("src.main")
+require("dependency/dct")
+require("dependency/moose")
+
+
+local atc = FLIGHTCONTROL:New(AIRBASE.Afghanistan.Kandahar, 251, nil, "C:\\Program Files\\DCS-SimpleRadio-Standalone")
+atc:SetParkingGuardStatic("Static TAXI STOP")
+atc:Start()
